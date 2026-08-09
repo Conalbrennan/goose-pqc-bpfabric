8fd77f5758fe89ccb0c12927c22660f9b9d0fc58
Conalbrennan <123599625+Conalbrennan@users.noreply.github.com>
2026-08-09 12:12:58 +0100

    Update kem_key_server.py

--- scripts/kem_key_server.py
+++ scripts/kem_key_server.py
@@ -30,9 +30,7 @@ AUTHORISED_GROUP_MEMBERS = {
     }
 }
 
-REVOKED_GROUP_MEMBERS = {
-    "goose-encryption-endpoint",
-}
+REVOKED_GROUP_MEMBERS = set()
 
 DEFAULT_HOST = "127.0.0.1"
 DEFAULT_PORT = 9000
