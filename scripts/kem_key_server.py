09d22a9f0a2038a38f8ab11908d0232787047a97
Conalbrennan <123599625+Conalbrennan@users.noreply.github.com>
2026-08-27 20:44:36 +0100

    Update kem_key_server.py to test aes128

--- scripts/kem_key_server.py
+++ scripts/kem_key_server.py
@@ -196,7 +196,7 @@ def derive_session_material(
 
     hkdf = HKDF(
         algorithm=hashes.SHA256(),
-        length=60,
+        length=52,
         salt=transcript_hash,
         info=b"goose-bpfabric-secure-kem-v1",
     )
@@ -204,9 +204,9 @@ def derive_session_material(
     derived_material = hkdf.derive(shared_secret)
 
     return (
-        derived_material[0:24],
-        derived_material[24:56],
-        derived_material[56:60],
+        derived_material[0:16],
+        derived_material[16:48],
+        derived_material[48:52],
     )
 
 
