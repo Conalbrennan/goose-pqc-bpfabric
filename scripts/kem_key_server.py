816413acbade87e7e7af03146d9b985298c31dd2
Conalbrennan <123599625+Conalbrennan@users.noreply.github.com>
2026-08-29 11:48:23 +0100

    Change key algorithm to 512

--- scripts/kem_key_server.py
+++ scripts/kem_key_server.py
@@ -17,7 +17,7 @@ import oqs
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
-KEM_ALGORITHM = "ML-KEM-768"
+KEM_ALGORITHM = "ML-KEM-512"
 SIGNATURE_ALGORITHM = "ML-DSA-44"
 PROTOCOL_VERSION = 1
 
